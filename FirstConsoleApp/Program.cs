db0cd39b4609a7b6318762e8c4f437bf79d94202
CarlyXoX <cjanscha@my.wctc.edu>
2025-02-05 17:06:05 -0800

    I accidently did the assinment in a local version of the class project

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Security.Cryptography.X509Certificates;
+
+//Syntax errors and formatting issues solved with ChatGPT
+namespace FirstConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("1. Display Characters");
+            Console.WriteLine("2. Add Character");
+            Console.WriteLine("3. Level Up Character");
+            var input = Console.ReadLine();
+            int.TryParse(input, out int opt);
+
+            StreamReader sr = new StreamReader("input.csv");
+            StreamWriter sw = new StreamWriter("input.csv", true);
+
+            string[] classes = new string[]
+            {
+                "Barbarian", "Bard", "Cleric", "Druid", "Fighter",
+                "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer",
+                "Warlock", "Wizard", "Artificer"
+            };
+
+            void WriteClasses()
+            {
+                for (int i = 0; i < classes.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {classes[i]}");
+                }
+            }
+
+            string ChooseClass(int classChoice)
+            {
+                if (classChoice >= 1 && classChoice <= classes.Length)
+                {
+                    return classes[classChoice - 1];
+                }
+                return "Unknown";
+            }
+
+            switch (opt)
+            {
+                case 1:
+                    Console.WriteLine("Displaying Characters:");
+                    Console.WriteLine(sr.ReadToEnd());
+                    sr.Close();
+                    break;
+                case 2:
+                    Console.Write("Name: ");
+                   string name = Console.ReadLine();
+                    
+                    WriteClasses();
+                    Console.Write("Choose a class: ");
+                    int.TryParse(Console.ReadLine(), out int classChoice);
+                    string chosenClass = ChooseClass(classChoice);
+                    int lvl = 1;
+                    int hp = 10;
+                    string inventory = "Empty";
+                    
+                    sw.WriteLine($"{name},{chosenClass},{lvl},{hp},{inventory}");
+                    sw.Close();
+                    Console.WriteLine("Character added successfully!");
+                    break;
+                case 3:
+                    Console.Write("Enter Current HP: ");
+                    int.TryParse(Console.ReadLine(), out int currentHP);
+                    
+                    Console.Write("Enter how many HP you wish to add to your stats: ");
+                    int.TryParse(Console.ReadLine(), out int increase);
+                    
+                    currentHP += increase;
+                    Console.WriteLine($"Updated HP: {currentHP}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+    }
+}
